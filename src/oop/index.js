df2a1a2c473d43314aae677c3dc3876e8ad39e37
Tuba ARSLAN <arslanawzentuba1@gmail.com>
2021-10-23 15:00:13 +0300

    son durum uygulaması yapıldı

--- src/oop/index.js
+++ src/oop/index.js
@@ -1,8 +1,29 @@
 class Customer{
     constructor(id,customerNumber){
-
+        this.instance_id = id
+        this.instance_customerNumber = customerNumber
     }
 
 }
 
-let customer = new Customer()  
+let customer = new Customer("21KE3","78432")
+
+Customer.bisey ="Mehmut"
+console.log(Customer.bisey)
+
+console.log("Ürün id: " + customer.instance_id)
+console.log("Ürün numarası: " + customer.instance_customerNumber)
+
+class IndividualCustomer extends Customer{
+    constructor(firstName,id,customerNumber){
+        super(id,customerNumber)
+        this.firstName = firstName
+    }
+}
+
+class CorporateCustomer extends Customer{
+    constructor(companyName,id,customerNumber){
+        super(id,customerNumber)
+        this.companyName = companyName
+    }
+}
